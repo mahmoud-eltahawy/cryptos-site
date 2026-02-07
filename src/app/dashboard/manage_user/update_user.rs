2dd14695ccd0e0b2bf769cc7c11741c4fbbe85e2
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-02-07 09:58:25 +0200

    refactor user update

--- src/app/dashboard/manage_user/update_user.rs
+++ src/app/dashboard/manage_user/update_user.rs
@@ -2,6 +2,7 @@ use leptos::prelude::*;
 use leptos_router::hooks::use_params_map;
 use uuid::Uuid;
 
+use crate::app::SecureUser;
 use crate::app::dashboard::get_user_by_id;
 use crate::auth::{AuthRequired, Level};
 
@@ -47,10 +48,6 @@ async fn update_level(target_id: uuid::Uuid, level: Level) -> Result<(), ServerF
 
 #[component]
 pub fn UpdateUser() -> impl IntoView {
-    let update_name = ServerAction::<UpdateName>::new();
-    let update_password = ServerAction::<UpdatePassword>::new();
-    let update_level = ServerAction::<UpdateLevel>::new();
-
     let params = use_params_map();
     let target_id = move || params.with(|p| p.get("targetId"));
     let target_res = Resource::new(
@@ -67,61 +64,92 @@ pub fn UpdateUser() -> impl IntoView {
 
     view! {
         <AuthRequired>
-            <div class="grid grid-cols-1 gap-5 text-center border-5 rounded-lg my-10 mx-5 p-1 md:p-3 lg:p-5">
-                <ActionForm action={update_name}>
-                    <input class="hidden" type="text" value={target_id} name="target_id"/>
-                    <div class="grid grid-cols-1 gap-2 my-10">
-                        <input
-                            class="text-center w-full px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 md:border-green-400"
-                            type="text"
-                            name="name"
-                            id="name"
-                            value={move || target().map(|x| x.name)}
-                        />
-                        <input
-                            class="w-auto px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                            type="submit"
-                            value="تحديث اسم المستخدم"
-                        />
+            <Suspense>
+                <ShowLet
+                    some=target
+                    let(SecureUser{id,name,level})
+                >
+                    <div class="grid grid-cols-1 gap-5 text-center border-5 rounded-lg my-10 mx-5 p-1 md:p-3 lg:p-5">
+                        <UpdateName id name/>
+                        <UpdateLevel id level/>
+                        <UpdatePassword id/>
                     </div>
-                </ActionForm>
-                <ActionForm action={update_level}>
-                    <input class="hidden" type="text" value={target_id} name="target_id"/>
-                    <div class="text-center grid grid-cols-1 gap-2 my-10">
-                        <select
-                            name="level"
-                            id="level"
-                            class="p-5 border-2 border-gray-300 rounded-lg text-center block text-sm font-bold mb-2 sm:text-base lg:text-xl"
-                        >
-                          <option value="Admin" selected={move || target().is_some_and(|x| matches!(x.level,Level::Admin))}>"ادمين"</option>
-                          <option value="User" selected={move || target().is_some_and(|x| matches!(x.level,Level::User))}>"مستخدم"</option>
-                        </select>
-                        <input
-                            class="w-auto px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                            type="submit"
-                            value="تحديث النفوذ"
-                        />
-                    </div>
-                </ActionForm>
-                <ActionForm action={update_password}>
-                    <input class="hidden" type="text" value={target_id} name="target_id"/>
-                    <div class="grid grid-cols-1 gap-2 my-10">
-                        <input
-                            class="text-center w-full px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 md:border-green-400"
-                            type="password"
-                            name="password"
-                            id="password"
-                        />
-                        <input
-                            class="w-auto px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                            type="submit"
-                            value="تحديث كلمة السر"
-                        />
-                    </div>
-                </ActionForm>
-            </div>
+                </ShowLet>
+            </Suspense>
         </AuthRequired>
         <GoBack/>
+    }
+}
+
+#[component]
+fn UpdatePassword(id: uuid::Uuid) -> impl IntoView {
+    let update_password = ServerAction::<UpdatePassword>::new();
+    view! {
+        <ActionForm action={update_password}>
+            <input class="hidden" type="text" value={id.to_string()} name="target_id"/>
+            <div class="grid grid-cols-1 gap-2 my-10">
+                <input
+                    class="text-center w-full px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 md:border-green-400"
+                    type="password"
+                    name="password"
+                    id="password"
+                />
+                <input
+                    class="w-auto px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    type="submit"
+                    value="تحديث كلمة السر"
+                />
+            </div>
+        </ActionForm>
+    }
+}
+
+#[component]
+fn UpdateLevel(id: uuid::Uuid, level: Level) -> impl IntoView {
+    let update_level = ServerAction::<UpdateLevel>::new();
+    view! {
+        <ActionForm action={update_level}>
+            <input class="hidden" type="text" value={id.to_string()} name="target_id"/>
+            <div class="text-center grid grid-cols-1 gap-2 my-10">
+                <select
+                    name="level"
+                    id="level"
+                    class="p-5 border-2 border-gray-300 rounded-lg text-center block text-sm font-bold mb-2 sm:text-base lg:text-xl"
+                >
+                  <option value="Admin" selected={matches!(level,Level::Admin)}>"ادمين"</option>
+                  <option value="User" selected={matches!(level,Level::User)}>"مستخدم"</option>
+                </select>
+                <input
+                    class="w-auto px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    type="submit"
+                    value="تحديث النفوذ"
+                />
+            </div>
+        </ActionForm>
+    }
+}
+
+#[component]
+fn UpdateName(id: uuid::Uuid, name: String) -> impl IntoView {
+    let update_name = ServerAction::<UpdateName>::new();
+    view! {
+        <ActionForm action={update_name}>
+            <input class="hidden" type="text" value={id.to_string()} name="target_id"/>
+            <div class="grid grid-cols-1 gap-2 my-10">
+                <input
+                    class="text-center w-full px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 md:border-green-400"
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                />
+                <input
+                    class="w-auto px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    type="submit"
+                    value="تحديث اسم المستخدم"
+                />
+            </div>
+        </ActionForm>
     }
 }
 
